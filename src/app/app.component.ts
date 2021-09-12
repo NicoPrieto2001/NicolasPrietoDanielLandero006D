28f84d8f2fc4f68936f74ec7473fba205479e99b
unknown <nico.prieto@duocuc.cl>
2021-09-12 15:30:06 -0300

    se ha implementado correctamente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+interface componente {
+  icon:string;
+  name:string;
+  redirecTo:string;
+}
+
+
+
+
+
+
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  componentes:componente[]=[
+    {
+      icon:'aperture-sharp',
+      name:'secundario',
+      redirecTo:'/secundario'
+      
+    },
+
+    {
+      icon:'car-sharp',
+      name:'terciario',
+      redirecTo:'/terciario'
+      
+    }
+
+
+
+
+
+  ]
   constructor() {}
 }
--- src/app/pages/primario/primario.page.ts
+++ src/app/pages/primario/primario.page.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MenuController } from '@ionic/angular';
+interface componente {
+  icon:string;
+  name:string;
+  redirecTo:string;
+}
 @Component({
   selector: 'app-primario',
   templateUrl: './primario.page.html',
   styleUrls: ['./primario.page.scss'],
 })
 export class PrimarioPage implements OnInit {
+  componentes:componente[]=[
+    {
+      icon:'aperture-sharp',
+      name:'secundario',
+      redirecTo:'/secundario'
+      
+    },
 
-  constructor() { }
+    {
+      icon:'car-sharp',
+      name:'terciario',
+      redirecTo:'/terciario'
+      
+    }
+
+
+
+
+
+  ]
+
+  constructor(private menuController:MenuController) { }
+  
 
   ngOnInit() {
   }
+  menu(){
+    this.menuController.open('first');
 
+  }
 }
